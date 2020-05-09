901ba07dccd1906ea6a55cdd2f5bef588104e652
deytola <adebolaadetola65@gmail.com>
2020-05-09 01:46:30 +0100

    refactor test authentication endpoint

--- src/test/java/com/springreact/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/springreact/cardatabase/CardatabaseApplicationTests.java
@@ -13,12 +13,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class CardatabaseApplicationTests {
 
-//	@Autowired
-//	private CarController carController;
-//
-//	@Test
-//	public void contextLoads() {
-//		assertThat(carController).isNotNull();
-//	}
+	@Autowired
+	private CarController carController;
+
+	@Test
+	public void contextLoads() {
+		assertThat(carController).isNotNull();
+	}
 
 }
